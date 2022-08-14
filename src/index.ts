01221f92e0bc639e1515cd6120d46b0c5d9c58ac
Willem Verbuyst <willemverbuyst@gmail.com>
2022-08-14 08:14:24 +0200

    feat: generate dummy patients on server

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import { PORT } from './constants'
+import { generateDummyData } from './dummyPatients'
 
 const app = express()
 
@@ -11,4 +13,11 @@ app.get('/', (_req, res) => {
   res.send({ message: 'hello world' })
 })
 
-app.listen(4000, () => console.log('hello world'))
+app.get('/patients', (_req, res) => {
+  const dummyPatients = Array.from({ length: 10 }).map(() =>
+    generateDummyData()
+  )
+  res.send({ status: 'success', data: dummyPatients })
+})
+
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
