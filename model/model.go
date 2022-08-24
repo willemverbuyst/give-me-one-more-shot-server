ce4bd0ece8188448ca871129c9de9af83f663ab3
Willem Verbuyst <willemverbuyst@gmail.com>
2022-08-24 05:23:45 +0200

    feat: create test model

---
+++ model/model.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type MessageID struct {
+	ID string `uri:"id" binding:"required"`
+}
+
+type Message struct {
+	ID        string         `gorm:"primaryKey" json:"id"`
+	Message   string         `json:"message" binding:"required"`
+	CreatedAt int64          `gorm:"autoCreateTime:milli" json:"created_at"`
+	UpdatedAt int64          `gorm:"autoUpdateTime:milli" json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at"`
+}
+
+func (x *Message) FillDefaults() {
+	if x.ID == "" {
+		x.ID = uuid.New().String()
+	}
+}
