2ee69b057da2455e5a1b0ccba515095ba64bf648
Willem Verbuyst <willemverbuyst@gmail.com>
2022-10-17 07:50:07 +0200

    docs: add swagger

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"give-me-one-more-shot/server/api/config"
 	"give-me-one-more-shot/server/api/routes"
+	_ "give-me-one-more-shot/server/docs"
 	"log"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
+// @title Give Me One More Shot API
+// @version         1.0
+// @description     Give Me One More Shot service API in Go using Gin framework.
+// @license.name  Apache 2.0
+// @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
+// @host      localhost:9090
 func main() {
 	db, err := config.Database()
 	if err != nil {
@@ -24,6 +33,8 @@ func main() {
 	routes.PatientRoute(router)
 	routes.UserRoute(router)
 	routes.RootRoute(router)
+
+	router.GET("/docs/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	sErr := router.Run(":9090")
 	if sErr != nil {
