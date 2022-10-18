884285918400ef1f6b429f286fecdf3736a5d508
Willem Verbuyst <willemverbuyst@gmail.com>
2022-10-18 05:45:41 +0200

    docs: add docs for user controllers

--- api/controllers/user_controller.go
+++ api/controllers/user_controller.go
@@ -9,12 +9,28 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// GetUsers godoc
+// @Summary      Get users array
+// @Description  Responds with the list of all users as JSON.
+// @Tags         users
+// @Produce      json
+// @Success      200 {object} responses.UsersResponse
+// @Router       /users [get]
 func GetUsers() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		context.IndentedJSON(http.StatusOK, responses.UsersResponse{Status: http.StatusOK, Message: "All users", Data: users})
 	}
 }
 
+// AddUser godoc
+// @Summary      Add User
+// @Description  Responds with the list of all users as JSON.
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        user body models.User true "Add user"
+// @Success      200 {object} responses.UsersResponse
+// @Router       /users [post]
 func AddUser() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		var u models.User
@@ -28,6 +44,16 @@ func AddUser() gin.HandlerFunc {
 	}
 }
 
+// GetUser godoc
+// @Summary      Get user by ID
+// @Description  Responds with a user as JSON
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        id path string true "User ID"
+// @Success      200 {object} responses.UserResponse
+// @Failure      404 {object} responses.HTTPError
+// @Router       /users/{id} [get]
 func GetUser() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		id := context.Param("id")
@@ -42,6 +68,16 @@ func GetUser() gin.HandlerFunc {
 	}
 }
 
+// UpdateUser godoc
+// @Summary      Toggles active status of user
+// @Description  Responds with a user as JSON
+// @Tags         users
+// @Accept       json
+// @Produce      json
+// @Param        id path string true "User ID"
+// @Success      200 {object} responses.UserResponse
+// @Failure      404 {object} responses.HTTPError
+// @Router       /users/{id} [patch]
 func UpdateUser() gin.HandlerFunc {
 	return func(context *gin.Context) {
 		id := context.Param("id")
